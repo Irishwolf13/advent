558b31683ddaf0350c8867f3637aa6bd8fcabb95
John Rooney <rooneyjohn@gmail.com>
2022-12-05 18:44:06 -0500

    Got it

--- day_3/day_3.js
+++ day_3/day_3.js
@@ -5,7 +5,13 @@ let upperCaseLetters = "A";
 let lowerPoint = 1;
 let upperPoint = 27;
 let totalScore = 0;
-let foundYou = [];
+let foundDuplicates = [];
+const fs = require('fs');
+const rawData = fs.readFileSync("sample.txt", 'utf-8');
+const dataArray = rawData.split("\n");
+let firstHalf = "";
+let secondHalf = "";
+let frank = {};
 
 function createPoints(myArray, myLetter, myNumber) {
     for (let i = 0; i < 25; i++) {
@@ -21,22 +27,42 @@ function createPoints(myArray, myLetter, myNumber) {
 }
 
 function findDuplicateItem(){
-    const fs = require('fs');
-    const rawData = fs.readFileSync("sample.txt", 'utf-8');
-    const dataArray = rawData.split("\n");
-    let firstHalf = "";
-    let secondHalf = "";
-
     for (i in dataArray) {
         const half = Math.ceil(dataArray[i].length / 2);
         firstHalf = dataArray[i].slice(0, half);
         secondHalf = dataArray[i].slice(half);
+        let breakHelper = true;
+
         for (i in firstHalf) {
-            console.log(firstHalf[i]);
+            if (breakHelper){
+                for (j in secondHalf){
+                    if (firstHalf[i] == secondHalf[j]) {
+                        foundDuplicates.push(secondHalf[j]);
+                        breakHelper = false;
+                        break
+                    }
+                }
+            }else {
+                break
+            }
+        }
+    }
+    console.log("Arra", foundDuplicates)
+    for (i in foundDuplicates) {
+        for (j in Points) {
+            if (foundDuplicates[i] == Points[j]) {
+                totalScore = totalScore + Points[parseInt(j) + 1]
+                console.log(totalScore)
+                break
+            }
         }
     }
 }
 
-//createPoints(Points, lowerCaseLetters, lowerPoint);
-//createPoints(Points, upperCaseLetters, upperPoint);
-findDuplicateItem();
+function threeLines() {
+    
+}
+createPoints(Points, lowerCaseLetters, lowerPoint);
+createPoints(Points, upperCaseLetters, upperPoint);
+//findDuplicateItem();
+threeLines();
