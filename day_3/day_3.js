3eec9d1060ca1e60290859c69553d0ed10123034
John Rooney <rooneyjohn@gmail.com>
2022-12-06 09:22:25 -0500

    Gold star Mother Fucker!

--- day_3/day_3.js
+++ day_3/day_3.js
@@ -7,7 +7,7 @@ let upperPoint = 27;
 let totalScore = 0;
 let foundDuplicates = [];
 const fs = require('fs');
-const rawData = fs.readFileSync("sample.txt", 'utf-8');
+const rawData = fs.readFileSync("input.txt", 'utf-8');
 const dataArray = rawData.split("\n");
 let firstHalf = "";
 let secondHalf = "";
@@ -64,28 +64,47 @@ function findDuplicateItem(){
 }
 
 function threeLines() {
-    for (i in dataArray[0]) {
-        set1.push(dataArray[0][i])
+    for (i in dataArray[myCount]) {
+        set1.push(dataArray[myCount][i])
     }
-    for (i in dataArray[1]) {
-        set2.push(dataArray[1][i])
+    myCount++;
+    for (i in dataArray[myCount]) {
+        set2.push(dataArray[myCount][i])
     }
-    for (i in dataArray[2]) {
-        set3.push(dataArray[2][i])
+    myCount++;
+    for (i in dataArray[myCount]) {
+        set3.push(dataArray[myCount][i])
     }
+    myCount++;
 
     const filteredArray1 = set1.filter(value => set2.includes(value));
-    // const filteredArray1 = set1.filter(function(value){
-    //     return set2.includes(value)
-    // });
     const filteredArray2 = filteredArray1.filter(value => set3.includes(value));
-    // const filteredArray2 = filteredArray1.filter(function(value) {
-    //     return set3.includes(value)
-    // });
-    console.log(filteredArray2)
+    //console.log(filteredArray2)
+    finalSet.push(filteredArray2[0])
+    set1 = [];
+    set2 = [];
+    set3 = [];
+}
 
+function getAllElves(){
+    const myLoops = Math.floor(dataArray.length/3)
+    console.log(myLoops)
+    for (let index = 0; index < myLoops; index++) {
+        threeLines();
+    }
+}
+
+function getTotalScore() {
+    for (i in finalSet) {
+        for (j in Points) {
+            if (finalSet[i] == Points[j]) {
+                totalScore = totalScore + Points[parseInt(j)+1]
+            }
+        }
+    }
 }
 createPoints(Points, lowerCaseLetters, lowerPoint);
 createPoints(Points, upperCaseLetters, upperPoint);
-//findDuplicateItem();
-threeLines();
+getAllElves();
+getTotalScore();
+console.log(totalScore);
